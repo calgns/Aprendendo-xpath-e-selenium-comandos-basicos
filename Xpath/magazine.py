b5ffbf1e4bf999812151ba8c3a96fbd9a359330c
calgns <matheus110903@gmail.com>
2021-08-10 13:51:40 -0300

    adicionando mais comentario explicando como ver o xpath e do que fazer

--- Xpath/magazine.py
+++ Xpath/magazine.py
@@ -1,5 +1,14 @@
 # necessário baixar o webdriver atual
 # link = https://chromedriver.chromium.org/downloads
+# para você ver o Xpath voce pode usar a tecla f12 e depois ctrl + shift + c
+# ou ctrl + shift + i, porém, a melhor opção é ctrl + shift + c pois
+# ja coloca  as informações no mouse facilitando na hora de procurar o xpath
 from selenium import webdriver
 #abre o navegador
 navegador = webdriver.Chrome()
+# coloque o site que ele vai abrir. Exemplo: https://www.youtube.com/
+# ou https://ri.magazineluiza.com.br/ (que esta send usado)
+# depois disso tudo escolher o site e baixar o driver, entre no site e copie seu endereço
+# e o cole assim como foi feito
+# isso vai abrir o site
+navegador.get("https://ri.magazineluiza.com.br/")
