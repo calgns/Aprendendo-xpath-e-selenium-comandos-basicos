42fa370b6238edac80732deea9587e5f320f81a0
calgns <matheus110903@gmail.com>
2021-08-10 15:10:56 -0300

    adicionando mais comentario, porem, colocando eles na nota para não atrapalhar na leitura do codigo

--- Xpath/magazine.py
+++ Xpath/magazine.py
@@ -1,14 +1,23 @@
-# necessário baixar o webdriver atual
-# link = https://chromedriver.chromium.org/downloads
-# para você ver o Xpath voce pode usar a tecla f12 e depois ctrl + shift + c
-# ou ctrl + shift + i, porém, a melhor opção é ctrl + shift + c pois
-# ja coloca  as informações no mouse facilitando na hora de procurar o xpath
 from selenium import webdriver
-#abre o navegador
+#abre o navegador automaticamente não se preucupe com a mensagem dizendo que ele estasendo controlado ate
+#porque é você quem esta o controlando.
 navegador = webdriver.Chrome()
 # coloque o site que ele vai abrir. Exemplo: https://www.youtube.com/
-# ou https://ri.magazineluiza.com.br/ (que esta send usado)
+# ou https://ri.magazineluiza.com.br/ (que esta sendo usado,
+# basicamente você vai pegar uma planilha, porem, forma automatizada)
 # depois disso tudo escolher o site e baixar o driver, entre no site e copie seu endereço
 # e o cole assim como foi feito
-# isso vai abrir o site
+# o get vai entrar no site
 navegador.get("https://ri.magazineluiza.com.br/")
+# aperte as teclas ctrl + shift + c e
+# utilizando o botão esquerdo do mouse você
+# clica encima do ( "x" ) para obter o caminho para fechar o anuncio
+# o meu deu
+# (de ctrl+c) //*[@id="owl-destaques"]/div[1]/div/div[4]/div/a/img
+#esse código ja no seu pode ser diferente principalmente se o site ja atualizou
+# então faça o favor de clicar
+#sempre com aspas simples de fora
+# com aspas duplas pode dar error
+#.find_element_by_xpath vai encontrar o xpath  e click no final va
+navegador.find_element_by_xpath('//*[@id="banner"]/a[1]/img').click() #lembrete.voce deve fechar o ANUNCIO para
+# continuar
